22d68d8a6d83295c8486b00574a6c79358d9e985
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-12-07 16:57:19 +0500

    Update index.blade.php

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -46,7 +46,7 @@
             </div>
             <div class="mt-4 flex md:mt-0 md:ml-4">
                 <!-- Add Button (We will build this page next) -->
-                <a href="#" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500">
+                <a href="{{ route('admin.universities.create') }}" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500">
                     <svg class="-ml-1 mr-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"/>
                     </svg>
