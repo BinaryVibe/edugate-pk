718c89cd53b59dae17435987876775357c02204a
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-12-07 16:19:00 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UniversityController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Homepage Route (Lists all universities)
+Route::get('/', [UniversityController::class, 'index'])->name('home');
+
+// Detail Page Route (We will build the view for this next)
+Route::get('/university/{university}', [UniversityController::class, 'show'])->name('universities.show');
